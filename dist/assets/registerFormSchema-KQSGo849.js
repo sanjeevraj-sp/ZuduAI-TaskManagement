00ce478d759fd9a7188c111533e0cb76d7122b8a
sanjeevraj-sp <sanjeevrajspkec2002@gmail.com>
2025-07-31 22:53:33 +0530

    Build Update

--- dist/assets/registerFormSchema-KQSGo849.js
+++
@@ -1,1 +0,0 @@
-import{J as r}from"./index-upvku8zh.js";const i={name:r.string().required(),email:r.string().email({tlds:{allow:!1}}).required(),password:r.string().min(5).required()};export{i as default};
